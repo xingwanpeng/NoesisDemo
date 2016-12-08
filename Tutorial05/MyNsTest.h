ad0a1ccd41b676a0f5f9202dbf546b3a5f4e3f65
XingWP <xingwanpengxwp@163.com>
2016-12-09 00:16:06 +0800

    显示图片

--- Tutorial05/MyNsTest.h
+++ Tutorial05/MyNsTest.h
@@ -1,17 +1,19 @@
 #include <NoesisGUI.h>
 
-class MyNsTest : public Noesis::Page
+class MyNsTest : public Noesis::Grid
 {
 public:
 	MyNsTest();
 	~MyNsTest();
 	Noesis::Grid *_title;
 	Noesis::Image *_curImage;
+	Noesis::Ptr<Noesis::ImageBrush> _brush;
 	void MyNsTest::OnInit(Noesis::BaseComponent *sender, const Noesis::EventArgs& event);
 	void MyNsTest::createTitle();
 
-	NS_IMPLEMENT_INLINE_REFLECTION(MyNsTest, Noesis::Page)
+	NS_IMPLEMENT_INLINE_REFLECTION(MyNsTest, Noesis::Grid)
 	{
 		NsMeta<Noesis::TypeId>("MyNsTest");
+		NsProp("Brush", &MyNsTest::_brush);
 	}
 };
